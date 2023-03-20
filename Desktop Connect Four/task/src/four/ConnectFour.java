50c7a3732c0aa8c8828671c41e7219b331db60f6
Varganum <nikolai.sobolev@gmail.com>
2023-03-21 02:44:07 +0600

    Stage 3

--- Desktop Connect Four/task/src/four/ConnectFour.java
+++ Desktop Connect Four/task/src/four/ConnectFour.java
@@ -4,13 +4,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class ConnectFour extends JFrame {
 
-    private static final int ROWS_NUMBER = 6;
-    private static final int COLUMNS_NUMBER = 7;
+    public static final int ROWS_NUMBER = 6;
+    public static final int COLUMNS_NUMBER = 7;
 
     private boolean isTurnX = true;
+    private ColumnChecker checker;
     public ConnectFour() {
         super("Connect Four");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,10 +21,12 @@ public class ConnectFour extends JFrame {
         //setTitle("Connect Four");
 
         Cell[][] gameCells = new Cell[ROWS_NUMBER][COLUMNS_NUMBER];
+        checker = new ColumnChecker(gameCells);
         char columnChar;
+
         for (int i = ROWS_NUMBER; i > 0; i--) {
             for (int j = 0; j < COLUMNS_NUMBER; j++) {
-                gameCells[i - 1][j] = new Cell(this);
+                gameCells[i - 1][j] = new Cell(this, j, i - 1);
                 columnChar = (char) ('A' + j);
                 //gameCells[i - 1][j].setText(Character.toString(columnChar) + i);
                 gameCells[i - 1][j].setText(" ");
@@ -42,27 +46,66 @@ public class ConnectFour extends JFrame {
         this.isTurnX = turn;
     }
 
+    public ColumnChecker getChecker() {
+        return checker;
+    }
 }
 
 class Cell extends JButton implements ActionListener {
 
-    ConnectFour game;
+    private final ConnectFour game;
+    private final int column;
+    private final int raw;
 
-    public Cell(ConnectFour game) {
+
+    public Cell(ConnectFour game, int column, int raw) {
         super();
         setFocusPainted(false);
         this.addActionListener(this);
         this.game = game;
+        this.column = column;
+        this.raw = raw;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (game.getIsTurnX()) {
-            this.setText("X");
-            game.setIsTurnX(false);
-        } else {
-            this.setText("O");
-            game.setIsTurnX(true);
+        Cell cellToFill = game.getChecker().findCellToFill(this.getColumn());
+        if (Objects.nonNull(cellToFill)) {
+            if (game.getIsTurnX()) {
+                cellToFill.setText("X");
+                game.setIsTurnX(false);
+            } else {
+                cellToFill.setText("O");
+                game.setIsTurnX(true);
+            }
         }
     }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public int getRaw() {
+        return raw;
+    }
+}
+
+class ColumnChecker {
+    private Cell[][] field;
+
+    ColumnChecker(Cell[][] field) {
+        this.field = field;
+    }
+
+    public Cell findCellToFill (int column) {
+        Cell result = null;
+        for (int i = 1; i <= ConnectFour.ROWS_NUMBER; i++ ) {
+            if (field[i - 1][column].getText().isBlank()) {
+                result = field[i - 1][column];
+                break;
+            }
+        }
+        return result;
+    }
+
 }
